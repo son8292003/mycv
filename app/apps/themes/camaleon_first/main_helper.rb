89bf3bafd33efe6329239159b69a6ee9dc026505
son8292003 <son8292003@gmail.com>
2017-12-23 16:46:10 +1100

    modified theme to add personal details fields, create profession skills post type when theme is installed.

--- app/apps/themes/camaleon_first/main_helper.rb
+++ app/apps/themes/camaleon_first/main_helper.rb
@@ -20,10 +20,37 @@ module Themes::CamaleonFirst::MainHelper
     group.add_field({"name"=>"Recent items from", "slug"=>"recent_post_type"}, {field_key: "select_eval", command: "camaleon_first_list_select"})
     group.add_field({"name"=>"Maximum of items", "slug"=>"home_qty"}, {field_key: "numeric", default_value: 6})
 
+    group = theme.add_field_group({name: "Personal Details", slug: "personal_details"})
+    group.add_field({"name"=>"Name", "slug"=>"pd_name"}, {field_key: "editor", default_value: "Your Name"})
+    group.add_field({"name"=>"Job title", "slug"=>"pd_job_title"}, {field_key: "editor", default_value: "job title"})
+    group.add_field({"name"=>"Name on table", "slug"=>"pd_name_on_table"}, {field_key: "editor", default_value: "Your Name"})
+    group.add_field({"name"=>"Date of birth", "slug"=>"pd_dob"}, {field_key: "editor", default_value: "01/01/2001"})
+    group.add_field({"name"=>"Phone", "slug"=>"pd_phone"}, {field_key: "editor", default_value: "+999 98765432"})
+    group.add_field({"name"=>"Email", "slug"=>"pd_email"}, {field_key: "editor", default_value: "no-reply@somedomainname.com"})
+    group.add_field({"name"=>"Description", "slug"=>"pd_description"}, {field_key: "editor", default_value: "Some description about yourself....Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem."})
+
     group = theme.add_field_group({name: "Footer", slug: "footer"})
-    group.add_field({"name"=>"Column Left", "slug"=>"footer_left"}, {field_key: "editor", translate: true, default_value: "<h4>My Bunker</h4><p>Some Address 987,<br> +34 9054 5455, <br> Madrid, Spain. </p>"})
-    group.add_field({"name"=>"Column Center", "slug"=>"footer_center"}, {field_key: "editor", translate: true, default_value: "<h4>My Links</h4> <p><a href='#'>Dribbble</a><br> <a href='#'>Twitter</a><br> <a href='#'>Facebook</a></p>"})
-    group.add_field({"name"=>"Column Right", "slug"=>"footer_right"}, {field_key: "editor", translate: true, default_value: "<h4>About Theme</h4><p>This cute theme was created to showcase your work in a simple way. Use it wisely.</p>"})
+    group.add_field({"name"=>"Column Left", "slug"=>"footer_left"}, {field_key: "editor", translate: true, default_value: "<h4>My Contacts</h4><p>Some Address 987,<br> +34 9054 5455 </p>"})
+    group.add_field({"name"=>"Column Center", "slug"=>"footer_center"}, {field_key: "editor", translate: true, default_value: "<h4>Link to this project:</h4> <p> <br> <a href='https://github.com/son8292003/mycv'>mycv</a></p>"})
+    group.add_field({"name"=>"Column Right", "slug"=>"footer_right"}, {field_key: "editor", translate: true, default_value: "<h4>About Theme</h4><p>Modified from camaleon_first theme.</p>"})
+
+
+    profession_pt = current_site.post_types.create({name: 'Professional_Skills', slug: 'Professional_skills'.to_s.parameterize, description: 'Professional', data_options: {has_category: false, has_tags: false, not_deleted: true, has_summary: false, has_content: true, has_comments: false, has_picture: false, has_template: false}})
+
+    profession_pt.add_post(title: "skill1", post_order: 1, content: "skill 1 description");
+    profession_pt.add_post(title: "skill2", post_order: 2, content: "skill 2 description");
+    profession_pt.add_post(title: "skill3", post_order: 3, content: "skill 3 description");
+    profession_pt.add_post(title: "skill4", post_order: 4, content: "skill 4 description");
+=begin
+    extra_post_type = [
+           {name: 'Personal_Details', description: 'Personal Details', options: {has_category: false, has_tags: false, not_deleted: true, has_summary: false, has_content: true, has_comments: false, has_picture: false, has_template: false }}
+       ]
+
+    extra_post_type.each do |pt|
+         model_pt = current_site.post_types.create({name: pt[:name], slug: pt[:name].to_s.parameterize, description: pt[:description], data_options: pt[:options]})
+         model_pt.add_field({name: "Name", slug: "personal_name"}, {field_key: "text_box", translate: false})
+    end
+=end
 
   end
 
